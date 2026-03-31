c539bbbbbb1778375a608d990547a066b2cd895f
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-03-31 19:06:16 +0200

    feat: add cd command for shell integration (eval $(ggg cd <name>))

---
+++ cmd/cd.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+
+	"go-git-get/config"
+	"go-git-get/repo"
+
+	"github.com/spf13/cobra"
+)
+
+var cdCmd = &cobra.Command{
+	Use:   "cd <name>",
+	Short: "Print the path of a repository (use with eval: eval $(ggg cd <name>))",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.Load()
+		if err != nil {
+			return err
+		}
+
+		repos, err := filterRepo(cfg.Repos, args[0])
+		if err != nil {
+			return err
+		}
+
+		fullPath, err := repo.FullPath(cfg.BaseDir, repos[0])
+		if err != nil {
+			return err
+		}
+
+		if !repo.IsCloned(fullPath) {
+			return fmt.Errorf("repository is not cloned: %s", fullPath)
+		}
+
+		fmt.Printf("cd %s\n", fullPath)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cdCmd)
+}
